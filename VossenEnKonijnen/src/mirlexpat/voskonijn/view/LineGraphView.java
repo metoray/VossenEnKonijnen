90b5bb60fea5ee4c91e25b9fa26d5ef5af8d8d0c
Lex <lexh94@hotmail.com>
2015-02-01 21:01:06 +0100

    More Javadocs.

--- VossenEnKonijnen/src/mirlexpat/voskonijn/view/LineGraphView.java
+++ VossenEnKonijnen/src/mirlexpat/voskonijn/view/LineGraphView.java
@@ -9,9 +9,14 @@ import java.util.Map;
 import mirlexpat.voskonijn.logic.Counter;
 import mirlexpat.voskonijn.logic.Field;
 import mirlexpat.voskonijn.logic.Simulator;
-
+/**
+ * Renders a linegraph on screen, which changes according to the simulator.
+ * 
+ * @author Mirko Rog
+ * @version 1.0
+ */
 public class LineGraphView extends GraphView {
-	
+
 	private HashMap<Class,History> histories;
 	private int lastStep;
 
@@ -20,7 +25,10 @@ public class LineGraphView extends GraphView {
 		histories = new HashMap<Class,History>();
 		lastStep = -1;
 	}
-
+	/**
+	 * Renders the linegraph on screen.
+	 * Scales according to the screen the linegraph is placed in.
+	 */
 	@Override
 	protected void render(Graphics g, Field field) {
 		scaleLine();
@@ -29,7 +37,7 @@ public class LineGraphView extends GraphView {
 			histories.clear();
 			lastStep = -1;
 		}
-		
+
 		for(Counter ctr: field.getStats().getCounters()){
 			Class cls = ctr.getClazz();
 			int count = ctr.getCount();
@@ -54,20 +62,26 @@ public class LineGraphView extends GraphView {
 			}
 			g.drawString(""+values[values.length-1], 160, h-getScaled(values[values.length-1]));
 		}
-		
+
 		lastStep = field.getStep();
 	}
-	
+	/**
+	 * Scales the view to the size of the application window.
+	 * @param n
+	 * @return 0
+	 */
 	private int getScaled(int n){
 		if(n>0) {
 			return (int)((Math.log(n)/Math.log(2))*8);
 		}
 		return 0;
 	}
-	
+	/**
+	 * Stores the previous step's values in a variable to draw an accurate line.
+	 */
 	private class History{
 		int[] history;
-		
+
 		public History(int size){
 			history = new int[size];
 			for(int i=0; i<size; i++){
@@ -75,14 +89,20 @@ public class LineGraphView extends GraphView {
 			}
 
 		}
-		
+		/**
+		 * Adds a new value to the variable explained in History's Javadocs.
+		 * @param amount that needs to be added
+		 */
 		public void add(int amount){
 			for(int i=0; i<history.length-1;i++){
 				history[i] = history[i+1];
 			}
 			history[history.length-1] = amount;
 		}
-		
+		/**
+		 * Returns the amount currently stored in history.
+		 * @return amount currently stored in history
+		 */
 		public int[] getHistory(){
 			return history;
 		}
